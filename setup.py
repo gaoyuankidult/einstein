67113ad35ed3b1758f2c40edb4219a89e21fa142
gao <gaoyuankidut@gmail.com>
2015-05-07 11:00:12 +0300

    structure change

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='einstein',
+      version='3.',
+      description='Deep Learning Package for Robot Learning',
+      author='Yuan Gao',
+      author_email='gaoyuankidult@gmail.com',
+      url='',
+      packages=['einstein'],
+     )
